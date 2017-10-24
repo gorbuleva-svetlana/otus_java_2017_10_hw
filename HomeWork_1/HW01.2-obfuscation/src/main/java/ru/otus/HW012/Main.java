677b0e7e0ae8111af404de31fc02736c87ccc829
gorbuleva-svetlana <gegsa08@gmail.com>
2017-10-24 21:48:14 +0600

    fixed errors

--- HomeWork_1/HW01.2-obfuscation/src/main/java/ru/otus/HW012/Main.java
+++ HomeWork_1/HW01.2-obfuscation/src/main/java/ru/otus/HW012/Main.java
@@ -1,4 +1,4 @@
-package ru.otus.HW011;
+package ru.otus.HW012;
 
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
@@ -9,10 +9,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> list = Lists.newArrayList(Splitter.on(',')
-                        .trimResults(CharMatcher.is('|'))
-                        .omitEmptyStrings()
-                        .split("|1abc,|2|,,4cd|,|5 c d e |,def6")
-                );
+                .trimResults(CharMatcher.is('|'))
+                .omitEmptyStrings()
+                .split("|1abc,|2|,,4cd|,|5 c d e |,def6")
+        );
         list.forEach(System.out::println);
- }
+    }
 }
